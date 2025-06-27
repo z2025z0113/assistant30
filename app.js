7bcb99bafe981cfa58dd0c7cfebebd07aa771e97
Your Name2025 <z2025z0113@gmail.com>
2025-06-27 15:04:16 +0800

    ch

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 3000;
-
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('assistant.db')
+db.run(`create table if not exists money_variation(
+  id integer primary key autoincrement,
+  thing text not null,
+  money double default 0,
+  happen timestamp not null
+  )`)
+  db.close()
 app.use(express.static("public")); // Serves files from ./public/
 // Serve index.html for root path
 app.get("/", (req, res) => {
