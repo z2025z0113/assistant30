195e726491818310e0186b5e6c6cb5973d6bc598
Your Name2025 <z2025z0113@gmail.com>
2025-06-28 20:28:20 +0800

    quick

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 3000;
-const sqlite3 = require('sqlite3').verbose()
-const db = new sqlite3.Database('assistant.db')
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("assistant.db");
 db.run(`create table if not exists money_variation(
   id integer primary key autoincrement,
   thing text not null,
   money double default 0,
   happen timestamp not null
-  )`)
-  db.close()
+  )`);
+db.run(`insert into money_variation(thing,money,happen) values(?,?,?)`,['buy 500g apple online',-28.62,'2025-6-28 20:27'])
+db.close();
 app.use(express.static("public")); // Serves files from ./public/
 // Serve index.html for root path
 app.get("/", (req, res) => {
